d6118459af72b407494b3ec60644689d79370a1c
KevinQbds11 <kcastillo93@uan.edu.co>
2022-09-22 15:49:14 -0500

    commit

--- src/app/routes/user.js
+++ src/app/routes/user.js
@@ -1,7 +1,7 @@
+
 const dbConnection = require('../../config/dbConnection');
 module.exports = app => {
     const connection = dbConnection();
-
 
     app.get('/', (req, res) => {
         connection.query('SELECT * FROM user', (err, result) => {
